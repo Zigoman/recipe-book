8b78f8f1c3d4c25678cd9b4ef62f08ec58baf44a
Nir Zigdon <Zigoman@users.noreply.github.com>
2017-05-27 02:05:15 +0300

    first Zigo UpDate2

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,3 +1,13 @@
-/**
- * Created by Zigo on 26/05/2017.
- */
+export class Recipe {
+  public name: string;
+  public description: string;
+  public imagePath: string;
+
+  constructor(name: string, desc: string, imagePath: string) {
+    this.name = name;
+    this.description = desc;
+    this.imagePath = imagePath;
+  }
+}
+
+
